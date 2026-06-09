8d325ea3308561ac3ddd4c3390487afabb4ab7d4
vitor nery <vitornerysantos24@gmail.com>
2026-06-09 18:24:22 -0300

    feat: criando a função gerador de qr code

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 import qrcode 
 import time 
 
+def gerar_qr_code(link,nome_arquivo):
+    print(f'Aguarde o qr code {nome_arquivo} esta sendo gerado..')
+    time.sleep(3)
+
+    qr = qrcode.make(link)
+
+    qr.save(f'{nome_arquivo}.png')
+
+    print(f'QR code {nome_arquivo} criado com sucesso!!')
+
+
+
 print('Seja bem vindo ao gerador de qr code')
 
 link = input('Envie o link que você quer transforar em qr code:')
+
 nome_arquivo = input('Digite o nome que seu arquivo será salvo:')
-print('aguarde, seu qr code esta sendo gerado')
-time.sleep(3)
 
-qr = qrcode.make(link)
-
-qr.save(f'{nome_arquivo}.png')
-
-print('QR code criado com sucesso!!')
+gerar_qr_code(link, nome_arquivo)
